00cdf25e97395ea6fb8ff0a6f7b51a733000d0a5
vevalentino24 <ve.febrian@gmail.com>
2015-02-18 02:35:45 +0700

    test commit

--- app/routes.php
+++ app/routes.php
@@ -34,7 +34,7 @@ Route::group(array('before' => 'auth'), function() {
 	});
 	Route::get('/park', function()
 	{
-		return View::make('park/home');
+		return View::make('park/home/test');
 	});
 
 	//user route
