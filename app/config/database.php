21bfc4594ce5f4d437b9c3d26a5aa55fa9c2f3ca
vevalentino24 <ve.febrian@gmail.com>
2015-03-25 16:56:42 +0700

    test

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'hidden_park',
 			'username'  => 'root',
-			'password'  => 'bozgenx3102',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
