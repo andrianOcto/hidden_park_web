9639e9f82fcdfaa514cc216e52789fd95303f9d6
Deny Ervan <deny.ervan@gmail.com>
2015-03-27 13:45:30 +0000

    modal delete confirmation

--- app/views/user/home.blade.php
+++ app/views/user/home.blade.php
@@ -25,11 +25,26 @@
             <td class="text-center">
                 <a href="user/{{ $value->iduser }}"><button type="button" class="btn btn-info btn-sm"><span class="glyphicon glyphicon-eye-open"></span> view</button></a>
                 <a href="user/{{ $value->iduser }}/edit"><button type="button" class="btn btn-warning btn-sm"><span class="glyphicon glyphicon-pencil"></span> edit</button></a>
-                <a href="user/{{ $value->iduser }}/destroy"><button type="button" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-pencil"></span> delete</button></a>
+                <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modal-delete"><span class="glyphicon glyphicon-trash"></span> delete</button>
             </td>
         </tr>
         @endforeach
     </table>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="modal-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h5>are you sure want to delete this user?</h5>
+      </div>
+      <div class="modal-footer">
+        <a href="user/{{ $value->iduser }}/destroy"><button type="button" class="btn btn-danger btn-sm">delete</button></a>
+      </div>
+    </div>
+  </div>
 </div>
 
 @include('template/foot')
